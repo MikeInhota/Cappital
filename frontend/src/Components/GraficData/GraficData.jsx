8eb6ea6982ae5bd506bf11f5f0cd73de93492dcd
Victor <55414928+victorleles12@users.noreply.github.com>
2020-10-26 15:25:32 -0300

    victorleles
    Add funcionalidade grafics com google charts

---
+++ frontend/src/Components/GraficData/GraficData.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Chart } from "react-google-charts";
+
+
+
+function GraficData() {
+
+  const [data, setData] = [
+    ['Linguagens', 'Quantidade'],
+    ['React', 100],
+    ['Angula', 80],
+    ['Vue', 50],
+  ]
+
+  return (
+        <div>
+          <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="Bar"
+            data={[
+                ['1', 'entrada', 'Saida'],
+                ['2', 8175000, 8008000],
+                ['3', 3792000, 3694000],
+              ]}
+          />
+        </div>
+  );
+}
+
+export default GraficData;
--- frontend/src/view/main/ObjectiveCard.jsx
+++ frontend/src/view/main/ObjectiveCard.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import "./Main.css";
+import GraficData from "../../Components/GraficData/GraficData"
+
 
 class ObjectiveCard extends Component {
   render() {
     return (
       <>
         <div className="container cartoes">
-          <div className="col-xl-2 col-md-10 mb-5">
+          <div className="col-xl-6 col-md-10 mb-5">
             <div className="card shadow h-100">
               <div className="card-body">
                 <div className="row no-gutters align-items-center meio-card">
@@ -17,9 +19,9 @@ class ObjectiveCard extends Component {
                   </div>
                   <div className="col-md mr-2">
                     <div className="text-xs font-weight-bold text-uppercase mb-1">
-                      Teste de conteudo
+                      <GraficData data={GraficData.data}/>
                     </div>
-                    <div className="h5 mb-0 font-weight-bold">Qualquer informação.</div>
+                    <div className="h5 mb-0 font-weight-bold">Movimentações</div>
                   </div>
                 </div>
               </div>
