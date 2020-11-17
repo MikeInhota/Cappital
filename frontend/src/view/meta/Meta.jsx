3160a79c04c5a26e84940f52ad2518ad68678924
Gabriela <gabriela.rocha71@etecia.com.br>
2020-11-16 22:03:11 -0300

    Alteração no grafico

--- frontend/src/view/meta/Meta.jsx
+++ frontend/src/view/meta/Meta.jsx
@@ -1,40 +1,64 @@
-import React, { PureComponent } from 'react';
+import React, { Component, PureComponent } from 'react';
 import './meta.css';
 import {  PieChart, Pie, Legend, Tooltip,} from 'recharts';
 
 const data01 = [
-  { name: 'Salario', value: 40 }, { name: 'Saldo', value:10 },
-  { name: 'Gastos', value: 300 }, 
-{ name: 'Meta', value: 0 }, ];
+  { name: 'Salario', value:25}, { name: 'Saldo', value:5 },
+  { name: 'Gastos', value:  25}, 
+  { name: 'Meta', value:20  },
+ ];
 
-const grafico= data01.map((value,index)=>({
- value:"" ,
- name:"",
-}));
+
 
 export default class Example extends PureComponent {
   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/k9jkog04/';
-  state = {
-    Salario: [],
-    Gastos: [],
-    Metas: [],
-    Saldo: [],
+  
+
+   mudarGrafico(datao1){
+ console.log(data01)
+   }
+
+ handleChangeSalario(event){
+  this.setState({Salario: event.target.value,})
   }
+handleChangeMeta(event){
+  this.setState({Meta: event.target.value,})
+  }
+handleChangeGastos(event){
+  this.setState({Gastos: event.target.value,})
+  }
+handleChangeSaldo(event){
+  this.setState({Saldo: event.target.value,})
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    this.props.mudarGrafico(this.state)
+      }
   render() {
-    return (<div className='Charts'>
+    return (
+      <>
+      <section>
+        <data01 mudarGrafico={this.mudarGrafico.bind(this)}/>
+      </section>
+    <section>
+    <div className='Charts'>
+        {data01.map(data01 =><data01 value={data01.value}/>)}
+        
         <h1>Meta de Gastos</h1>
-        <form>
-        <input name="salario" placeholder="Salario" type="number" />
-        <input name="meta" placeholder="Meta" type="number" />
-        <input name="gastos" placeholder="Gastos" type="number" />
-        <input name="saldo" placeholder="Saldo" type="number" />
-        </form>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+        <input name="salario" placeholder="Salario" type="number" onChange={this.handleChangeSalario.bind(this)}/>
+        <input name="meta" placeholder="Meta" type="number" onChange={this.handleChangeMeta.bind(this)}/>
+        <input name="gastos" placeholder="Gastos" type="number" onChange={this.handleChangeGastos.bind(this)}/>
+        <input name="saldo" placeholder="Saldo" type="number" onChange={this.handleChangeSaldo.bind(this)}/>
         <button>Salvar</button>
+        </form>
       <PieChart width={400} height={400}>
-       <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill=" #0B3073" label />
-       <Tooltip />
-      </PieChart></div>
-      
+      <Pie dataKey={this.props.value} isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill=" #0B3073" label />
+      <Tooltip />
+      </PieChart>
+      </div>
+      </section>
+      </>
     );
   }
 }
